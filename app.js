2f5158121a8fad6ce6936beb768a9f2f0e43a0b8
k_tith <khemtith@station29.com>
2023-04-24 21:42:29 +0700

    add homepage

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const express = require('express'); // Import the Express.js module
 const mysql = require('mysql');
+const path = require('path');
 
 // import knex to app
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig);
+
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 
@@ -15,12 +17,16 @@ global.knex = knex;
 const app = express(); // Create an Express.js app
 const router = express.Router(); // Create an instance of the router
 
+// Set up EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(bodyParser.json());
 app.use('/users', userRouter);
 app.use('/', router);
 
 app.get('/', (req, res) => {
-  res.send('Hello, My node js app!'); // Define a route handler
+  res.render('index', { title: 'My Homepage' });
 });
 
 const port = 3000; // Define the port number for your server
