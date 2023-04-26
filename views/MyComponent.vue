4ed0cab6b808e23f90e56426706416f4aea77a37
k_tith <tith@kiwigo.app>
2023-04-26 15:40:22 +0700

    add MVC to user

---
+++ views/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Hello {{ name }}!</h1>
+      <p>Welcome to my Node.js app with Vue.js.</p>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        name: 'world'
+      }
+    }
+  }
+</script>
+  
